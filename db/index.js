531489446ab59611b63e48cae7022284175a6b13
CataChavez <kata.chavez@gmail.com>
2021-04-21 22:27:20 -0400

    port:5000 in axios

--- db/index.js
+++ db/index.js
@@ -1,4 +1,9 @@
 const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL, ssl:{
+        rejectUnauthorized:false
+    }
+});
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
